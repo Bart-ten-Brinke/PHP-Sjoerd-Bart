a688318e77907ec2f78b4e9fe136b410a2cb65f4
karst050 <zuiderkruislaan11@gmail.com>
2020-06-13 12:01:24 +0000

    moi moi

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -14,18 +14,20 @@
     </head>
 
     <body>
-        <form method="POST" class="form-inline" action="php/login.php">
-            <input type="email" class="form-control mb-2 mr-sm-2" placeholder="e-mail" id="mail" name="mail">
-            <input type="password" class="form-control mb-2 mr-sm-2" placeholder="wachtwoord" id="pass" name="pass">
-            <button type="submit" class="btn btn-secondary mb-2">Login</button>
-        </form>
-        
-        <br>
-        <br>
+        <div class="inlogpagina">
+            <form method="POST" class="form-inline" action="php/login.php">
+                <input type="email" class="form-control mb-2 mr-sm-2" placeholder="e-mail" id="mail" name="mail">
+                <input type="password" class="form-control mb-2 mr-sm-2" placeholder="wachtwoord" id="pass" name="pass">
+                <button type="submit" class="btn btn-secondary mb-2">Login</button>
+            </form>
+            
+            <br>
+            <br>
 
-        <h3>Heeft u nog geen account?</h3>
-        <h4>Maak hier dan een account aan:</h4>
-        <a href="php/nieuwAccount.php">Nieuw account:</a>
+            <h3>Heeft u nog geen account?</h3>
+            <h4>Maak hier dan een account aan:</h4>
+            <a href="php/nieuwAccount.php">Nieuw account:</a>
+        </div>
     </body>
 </html>
 
