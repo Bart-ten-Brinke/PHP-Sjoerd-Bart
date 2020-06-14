c9ced44dc6d96960b58febde60521b49edd0bc38
Bart-ten-Brinke <150493@csgaugustinus.nl>
2020-06-14 10:54:06 +0000

    14 juni

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -15,7 +15,7 @@
 
     <body>
         <div class="inlogpagina">
-            <form method="POST" class="form-inline" action="php/login.php">
+            <form method="POST" class="login" action="php/login.php">
                 <input type="email" class="form-control mb-2 mr-sm-2" placeholder="e-mail" id="mail" name="mail">
                 <input type="password" class="form-control mb-2 mr-sm-2" placeholder="wachtwoord" id="pass" name="pass">
                 <button type="submit" class="btn btn-secondary mb-2">Login</button>
